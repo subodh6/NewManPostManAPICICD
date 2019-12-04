a07cd940f0a89979932323b09240a9e30ba32d22
Subodh Chowdary <Subodh.Chowdary@rsystems.com>
2019-12-04 15:36:43 +0530

    NewManPostManAPI

---
+++ GMAC/About.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+
+
+namespace GMAC
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
